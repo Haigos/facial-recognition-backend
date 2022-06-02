73df6f9e1d00790888dadef25b146ad3aa284892
haigos <timzurb@gmail.com>
2022-06-02 16:14:10 +0200

    database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'face-recognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
